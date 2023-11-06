e55e1f98d0d74f6c82c08f0b0c948ffdebca99dd
Leo Angelo Genota <genota.leo@gmail.com>
2023-11-06 15:00:18 -0500

    Creating a sample/generic template for user mods in Cities Skylines

---
+++ GenericModTemplate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using ICities;
+using UnityEngine;
+using ColossalFramework.Plugins;
+
+namespace GenericModTemplate
+{
+    public class GenericModTemplate : IUserMod
+    { 
+        public string Name {
+            get { return "Generic Mod Template"; }
+        }
+        public string Description {
+            get { return "A generic template for user mods for Cities Skylines."; }
+        }
+    }
+
+    public class Loader : LoadingExtensionBase
+    {
+        public override void OnLevelLoaded(LoadMode mode)
+        {
+            GameObject gameObject = new GameObject("sample object");
+            gameObject.AddComponent<SampleBehavior>();
+        }
+    }
+
+    public class SampleBehavior : MonoBehaviour
+    {
+        void Start()
+        {
+            DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, "Start Message!");
+        }
+
+        void Update()
+        {
+            DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, "Update Message!");
+        }
+    }
+}
